9484f63e17d7c4c36a8f753a1840f97c41d3cbae
Marvin de Leon <mrvndeleon0@gmail.com>
2017-02-23 18:46:08 -0600

    implemented basic list with dummy data

--- src/app/events/event-list/event-list.component.ts
+++ src/app/events/event-list/event-list.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'event-list',
-  templateUrl: 'app/events/event-list/event-list.component.html'
+    moduleId: module.id,
+    selector: 'event-list',
+    templateUrl: 'event-list.component.html'
 })
 export class EventListComponent  {
-  events: string[];
+    events: string[];
 
-  constructor () {
-    this.events = ["Birthday Party", "Cousin Niko's Wedding", "Bowling Night"];
-  }
+    constructor () {
+        this.events = ["Birthday Party", "Cousin Niko's Wedding", "Bowling Night"];
+    }
 }
