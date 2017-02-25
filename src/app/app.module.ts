1e65769cf82bba43256b963bd6ddab5f99db07bd
Marvin de Leon <mrvndeleon0@gmail.com>
2017-02-25 05:41:32 -0600

    refactored event-list into sub-component of event-view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { EventViewComponent } from './event-view/event-view.component';
 import { EventListComponent } from './event-view/event-list/event-list.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent, EventListComponent ],
+  declarations: [ AppComponent, EventViewComponent, EventListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
