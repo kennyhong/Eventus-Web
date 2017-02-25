c1bf6ddbc759e4b248391dae03d06df8a2d1b5dc
Marvin de Leon <mrvndeleon0@gmail.com>
2017-02-24 19:02:05 -0600

    implemented basic event class

--- src/app/events/event-list/event-list.component.ts
+++ src/app/events/event-list/event-list.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 
+import { Event } from '../shared/event';
+
 @Component({
     moduleId: module.id,
     selector: 'event-list',
     templateUrl: 'event-list.component.html'
 })
 export class EventListComponent  {
-    events: string[];
+    events: Event[];
 
     constructor () {
-        this.events = ["Birthday Party", "Cousin Niko's Wedding", "Bowling Night"];
+        this.events = [
+            {id: 1, name: "Hello", description: "World", date: "Today"},
+            {id: 2, name: "Foo", description: "Bar", date: "Tomorrow"},
+            {id: 3, name: "Steve", description: "Jobs", date: "In a bit"},
+            {id: 4, name: "Bill", description: "Cosby", date: "At a later date"}
+        ];
     }
 }
