7a1530dc91f0b20368677c920c2fe20ce7500878
Marvin de Leon <mrvndeleon0@gmail.com>
2017-02-25 04:29:55 -0600

    added missing "implements" keyword in EventListComponent class definition

--- src/app/events/event-list/event-list.component.ts
+++ src/app/events/event-list/event-list.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 import { Event } from '../shared/event.model';
 import { EventService } from '../shared/event.service';
 
-
 @Component({
     moduleId: module.id,
     selector: 'event-list',
     templateUrl: 'event-list.component.html'
 })
-export class EventListComponent  {
+export class EventListComponent implements OnInit {
     events: Event[];
     selectedEvent: Event;
 
