0b2d382e54b98ceb91f90a0d6712c69ef910206a
Marvin de Leon <mrvndeleon0@gmail.com>
2017-02-28 15:27:01 -0600

    fixed DI to properly inject MockEventService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { MockEventService } from './shared/mock/mock-event.service'
+import { EventService } from './shared/event.service';
+import { MockEventService } from './shared/mock/mock-event.service';
 
 @Component({
   moduleId: module.id,
   selector: 'eventus-app',
   templateUrl: 'app.component.html',
-  providers: [ MockEventService ] 
+  providers: [ {provide: EventService, useClass: MockEventService} ] 
 })
 export class AppComponent  { name = 'Angular'; }
--- src/app/event-view/event-view.component.ts
+++ src/app/event-view/event-view.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Event } from '../shared/event.model';
-import { MockEventService } from '../shared/mock/mock-event.service';
+import { EventService } from '../shared/event.service';
 import { EventListComponent } from './event-list/event-list.component';
 import { EventDetailComponent } from './event-detail/event-detail.component';
 
@@ -14,7 +14,7 @@ export class EventViewComponent implements OnInit {
     events: Event[];
     selectedEvent: Event;
 
-    constructor(private eventService: MockEventService) {}
+    constructor(private eventService: EventService) {}
 
     ngOnInit() {
         this.eventService.getEvents()
