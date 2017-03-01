57b9dd364f8182e335420fb1d90278073fddfd2e
Mitchell Engen <engenm@myumanitoba.ca>
2017-02-28 19:27:41 -0600

    added routes to app.module and created new-event html/ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { EventViewComponent } from './event-view/event-view.component';
 import { EventListComponent } from './event-view/event-list/event-list.component';
 import { EventDetailComponent } from './event-view/event-detail/event-detail.component';
+import { NewEventComponent } from './new-event-view/new-event.component';
+
+const appRoutes: Routes = [
+    {
+        path: 'home',
+        component: AppComponent
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: '#', 
+        component: EventListComponent
+    }
+];
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    BrowserModule
+  ],
   declarations: [
     AppComponent,
     EventViewComponent,
     EventListComponent,
-    EventDetailComponent
+    EventDetailComponent,
+    NewEventComponent
   ],
   bootstrap: [ AppComponent ]
 })
---
+++ src/app/new-event-view/new-event.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'new-event',
+    templateUrl: 'new-event.component.html',
+})
+export class NewEventComponent {}
