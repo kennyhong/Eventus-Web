89bd1992a7b07ab16afccc7f9f09a4f9f9963da5
Mitchell Engen <engenm@myumanitoba.ca>
2017-03-09 23:08:18 -0600

    new gh-pages version

--- src/app/new-event/new-event.component.ts
+++
@@ -1,8 +0,0 @@
-﻿import { Component } from '@angular/core';
-
-@Component({
-    moduleId: module.id,
-    selector: 'new-event',
-    templateUrl: 'new-event.component.html',
-})
-export class NewEventComponent { }
--- src/app/shared/service.model.ts
+++
@@ -1,8 +0,0 @@
-import { ServiceTag } from './service-tag.model';
-
-export class Service {
-    id: number;
-    name: string;
-    cost: number;
-    serviceTags: ServiceTag[];
-}
