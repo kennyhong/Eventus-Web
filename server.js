e2055c50200d6571630da282d40f2148ae2ad6a2
Mitchell Engen <engenm@myumanitoba.ca>
2017-03-10 23:30:12 -0600

    more heroku debugging

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/src'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
@@ -31,5 +31,5 @@ app.use(forceSSL());
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/src/index.html'));
 });
