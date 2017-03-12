5826b5c8f18b76a51344b8708deeee230fcafbf8
Mitchell Engen <engenm@myumanitoba.ca>
2017-03-11 20:35:20 -0600

    reverted mock event service back to actually being mock

--- src/app/shared/mock/mock-event.service.ts
+++ src/app/shared/mock/mock-event.service.ts
@@ -1,35 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
 
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
-
-import { Event } from '../event.model';
+import { EVENTS } from './mock-events';
 
 @Injectable()
 export class MockEventService {
-    private eventsUrl = 'http://eventus.us-west-2.elasticbeanstalk.com/api/events';  // URL to web API
-    constructor(private http: Http) { }
-    getEvents(): Observable<Event[]> {
-        return this.http.get(this.eventsUrl)
-            .map(this.extractData)
-            .catch(this.handleError);
-    }
-    private extractData(res: Response) {
-        let body = res.json();
-        return body.data || {};
-    }
-    private handleError(error: Response | any) {
-        let errMsg: string;
-        if (error instanceof Response) {
-            const body = error.json() || '';
-            const err = body.error || JSON.stringify(body);
-            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-        } else {
-            errMsg = error.message ? error.message : error.toString();
-        }
-        console.error(errMsg);
-        return Observable.throw(errMsg);
+    getEvents() {
+        return Promise.resolve(EVENTS);
     }
 }
