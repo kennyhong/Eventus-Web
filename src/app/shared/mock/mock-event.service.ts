fefe886e1d04655bf2816a6e2ebb9f05f2d15abe
Marvin de Leon <mrvndeleon0@gmail.com>
2017-03-14 22:28:01 -0500

    fixed compilation errors on mock-event

--- src/app/shared/mock/mock-event.service.ts
+++ src/app/shared/mock/mock-event.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of'
 
+import { Event } from '../event.model';
 import { EventService } from '../services/event.service';
 import { EVENTS } from './mock-events';
 
 @Injectable()
 export class MockEventService extends EventService {
-    getEvents() {
-        return new Observable<Event[]>();
-        // return Promise.resolve(EVENTS);
+    getEvents(): Observable<Event[]> {
+        return Observable.of(EVENTS);
     }
 }
