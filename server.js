583f796305b220dd9039035c008c1d46771e222f
Mitchell Engen <engenm@myumanitoba.ca>
2017-03-20 15:42:05 -0500

    removed unnecessary function from server

--- server.js
+++ server.js
@@ -13,20 +13,19 @@ app.listen(process.env.PORT || 8080);
 // a protocol other than HTTPS,
 // redirect that request to the
 // same url but with HTTPS
-const forceSSL = function () {
+/*const forceSSL = function () {
     return function (req, res, next) {
-        if (req.headers['x-forwarded-proto'] !== 'https') {
+        if (req.headers['x-forwarded-proto'] !== 'http') {
             return res.redirect(
-             ['https://', req.get('Host'), req.url].join('')
+             ['http://', req.get('Host'), req.url].join('')
             );
         }
         next();
     }
-}
+}*/
 // Instruct the app
 // to use the forceSSL
 // middleware
-//app.use(forceSSL());
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 app.get('/', function (req, res) {
