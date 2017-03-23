a8204c54d680c2c4c2a59b75f91f5d694588d25d
Marvin de Leon <mrvndeleon0@gmail.com>
2017-03-23 15:06:23 -0500

    added event service to component constructor

--- src/app/event-create/event-create.component.ts
+++ src/app/event-create/event-create.component.ts
@@ -1,8 +1,13 @@
 ﻿import { Component } from '@angular/core';
+import { EventService } from '../shared/services/event.service';
 
 @Component({
     moduleId: module.id,
     selector: 'event-create',
     templateUrl: 'event-create.component.html',
 })
-export class EventCreateComponent { }
+export class EventCreateComponent {
+    constructor(private eventService: EventService) {
+
+    }
+}
