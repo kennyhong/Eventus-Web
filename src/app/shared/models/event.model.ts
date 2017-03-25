902c23d7191214925be36ef7966b47bc0cc92da4
Marvin de Leon <mrvndeleon0@gmail.com>
2017-03-24 21:27:21 -0500

    Rewrote Event model and added Interface. Removed multiple constructors for Event model. Added interface for when creating new events.

--- src/app/shared/models/event.model.ts
+++ src/app/shared/models/event.model.ts
@@ -1,25 +1,15 @@
-import { Service } from './service.model'
+import { Service } from './service.model';
+
+/*
+This interface defines the properties that EventService expects when
+creating a new event as prescribed by the API
+ */
+export interface EventParams {
+    name: String;
+    description: String;
+    date: String;
+}
 
 export class Event {
-    id: number;
-    name: string;
-    description: string;
-    date: string;
-    services: Service[];
-
-    constructor() {
-        this.id = -1;
-        this.name = "";
-        this.description = "";
-        this.date = "";
-        this.services = [];
-    }
-
-    constructor(id: Number, name: String, description: String, date: String, services: Service[]) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.date = date;
-        this.services = services
-    }
+    constructor(id: Number, name: String, description: String, date: String, services: Service[]) {}
 }
