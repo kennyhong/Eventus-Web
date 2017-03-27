2e2cb50713067ed282efb3f273e3a055ad413dfc
Marvin de Leon <mrvndeleon0@gmail.com>
2017-03-27 12:10:55 -0500

    Moved event start time to a separate input.

--- src/app/event-create/event-create.component.ts
+++ src/app/event-create/event-create.component.ts
@@ -2,6 +2,13 @@
 import { Event, EventParams } from '../shared/models/event.model';
 import { EventService } from '../shared/services/event.service';
 
+interface EventCreateForm {
+    name: string;
+    description: string;
+    date: string;
+    time: string;
+}
+
 @Component({
     moduleId: module.id,
     selector: 'event-create',
@@ -9,21 +16,28 @@ import { EventService } from '../shared/services/event.service';
 })
 export class EventCreateComponent {
     submitted: boolean;
+    formData: EventCreateForm;
     eventParams: EventParams;
     createdEvent: Event;
 
     constructor(private eventService: EventService) {
         this.submitted = false;
+        this.formData = {
+            name: '',
+            description: '',
+            date: '',
+            time: ''
+        };
         this.eventParams = {name: '', description: '', date: ''};
         this.createdEvent = new Event(-1, '', '', '', []);
     }
 
     createEvent() {
         // TODO: give more user feedback that the event wasn't created
-        if (!this.validParams()) {
+        if (!this.validInput()) {
             return;
         }
-
+        this.parseForm();
         this.eventService.addEvent(this.eventParams)
             .subscribe(
                 event => {
@@ -34,21 +48,33 @@ export class EventCreateComponent {
             );
     }
 
-    private validParams(): boolean {
-        if (this.eventParams === undefined) {
+    private validInput(): boolean {
+        if (this.formData === undefined) {
             return false;
         } else {
-            for (let key in this.eventParams) {
-                if (this.eventParams[key] === '') {
+            for (let key in this.formData) {
+                if (this.formData[key] === '') {
+                    console.error(key + ' in formData is empty string');
                     return false;
                 }
             }
 
-            let pattern = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) ([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/;
-            if (!pattern.test(this.eventParams.date)) {
+            let datePattern = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
+            let timePattern = /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/;
+            if (!datePattern.test(this.formData.date)) {
+                console.error('Date does not match pattern');
+                return false;
+            } else if (!timePattern.test(this.formData.time)) {
+                console.error('Time does not match pattern');
                 return false;
             }
         }
         return true;
+    }
+
+    private parseForm() {
+        this.eventParams.name = this.formData.name;
+        this.eventParams.description = this.formData.description;
+        this.eventParams.date = this.formData.date + ' ' + this.formData.time;
     }
 }
