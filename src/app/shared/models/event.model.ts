2f96cf7344e9e56e9e7e1d0ffebe96f6fbeca636
Marvin de Leon <mrvndeleon0@gmail.com>
2017-03-26 22:06:09 -0500

    Changed types to JavaScript primitive types.

--- src/app/shared/models/event.model.ts
+++ src/app/shared/models/event.model.ts
@@ -5,19 +5,19 @@ This interface defines the properties that EventService expects when
 creating a new event as prescribed by the API
  */
 export interface EventParams {
-    name: String;
-    description: String;
-    date: String;
+    name: string;
+    description: string;
+    date: string;
 }
 
 export class Event {
-    id: Number;
-    name: String;
-    description: String;
-    date: String;
+    id: number;
+    name: string;
+    description: string;
+    date: string;
     services: Service[];
 
-    constructor(id: Number, name: String, description: String, date: String, services: Service[]) {
+    constructor(id: number, name: string, description: string, date: string, services: Service[]) {
         this.id = id;
         this.name = name;
         this.description = description;
